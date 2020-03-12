0cfd7a7ca7c25d6e9b5467adbd81b26d4a9d5659
jennysueee <56518268+jennysueee@users.noreply.github.com>
2020-03-11 20:14:20 -0400

    Add files via upload

--- plots.js
+++ plots.js
@@ -11,15 +11,15 @@ var layout = {
   title: "Restaraunt Grades in Manhattan - March 2020"
 };
 
-Plotly.newPlot('plot1', data, layout);
+Plotly.newPlot('plot', data, layout);
 
 //BAR CHART - TOP 10 VIOLATION CATEGORIES
 var trace1 = {
   x: ["Facility, machinery", 
-  "Food worker or worker cleaning responsibilities", 
-  "Food contamination via food, persons, animals", 
-  "Food temperature storage",
-  "Conditions conducive to pests or inadequate garbage removal"],
+  "Employee", 
+  "Food contamination", 
+  "Food temperature ",
+  "Pests / Garbage removal"],
   y: [25426, 16151, 12541, 8460, 8407],
   type: "bar"
 };
@@ -28,9 +28,9 @@ var data = [trace1];
 
 var layout = {
   title: "Top 5 DOH Violation Categories in Manhattan",
-  xaxis: { title: "Type of Violation"},
-  yaxis: { title: "# of Violations"},
-  automargin: true,
+  xaxis: { title: "Type of Violation", tickangle: 0, automargin : true },
+  yaxis: { title: "# of Violations", automargin : true},
+  //automargin: true,
 };
 
-Plotly.newPlot("plot2", data, layout);
+Plotly.newPlot("plot", data, layout);
